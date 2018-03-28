8f234c3311ae28e999b0ef2b11d649f5746bdb2d
beaczer <czernuszka.beata@gmail.com>
2018-03-28 14:44:32 +0200

    Klasy Modelu

---
+++ PraktyczeKursy/Models/Kategoria.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PraktyczeKursy.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategori { get; set; }
+        public string NazwaPlikuIkony{ get; set; }
+
+        public virtual ICollection<Kurs> Kursy { get; set; }
+    }
+}
---
+++ PraktyczeKursy/Models/PozycjaZamowienia.cs
@@ -0,0 +1,14 @@
+﻿namespace PraktyczeKursy.Models
+{
+    public class PozycjaZamowienia
+    {
+        public int PozycjaZamowieniaId { get; set; }
+        public  int ZamowieniaId { get; set; }
+        public  int KursId { get; set; }
+        public int Ilosc { get; set; }
+        public decimal CenaZakupy { get; set; }
+        
+        public virtual Kurs kurs { get; set; }
+        public virtual Zamowienia zamowienia { get; set; }
+    }
+}
